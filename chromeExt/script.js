5859eece80640dac474f9adcb25b4567f88cb72a
Jessie0611 <imjustme0611@hotmail.com>
2025-07-14 14:59:09 -0500

    Truthy/Falsy values

--- chromeExt/script.js
+++ chromeExt/script.js
@@ -1,11 +1,33 @@
+//STORING ARRAYS IN LOCAL STORAGE
+//everything in local storage has to be a string, use JSON.stringify() and JSON.prase()
+//JSON.stringify() - turns array and turns it into string / JSON.parse - fetch array and turn into string
+
+
+// let myLeads = ["www.awesomelead.com"]
+// myLeads = JSON.stringify(myLeads)
+// myLeads = JSON.parse(myLeads)
+// myLeads.push("www.epiclead.com")
+// console.log(myLeads)
+
+
+//let myLeads = `["www.awesomelead.com"]`
+// 1. Turn the myLeads string into an array:                            myLeads = JSON.parse(myLeads)
+// 2. Push a new value to the array:                                    myLeads.push("www.epiclead.com")
+// 3. Turn the array into a string again:                               myLeads = JSON.stringify(myLeads)
+// 4. Console.log the string using typeof to verify that it's a string: console.log(typeof myLeads)
+
+
+
+
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 // Get the leads from the localStorage - Store it in a variable, leadsFromLocalStorage
 // Log out the variable
-localStorage.clear()
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+// ["lead1", "lead2"] or null
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) //check if we have any leads in local storage, and prase to get them as JS array
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
