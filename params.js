606650cce2324ba931547e78489fb045f1d232fc
Jessie0611 <imjustme0611@hotmail.com>
2025-07-15 16:41:26 -0500

    numbers as arguments

--- params.js
+++ params.js
@@ -6,3 +6,11 @@ function greetUser(greeting, name, emoji) {
 
 
 greetUser("Hello", "Jessie", "👋")
+
+
+// Create a function, add(), that adds two numbers together and returns the sum
+function add(a, b) {
+    return a + b
+}
+console.log(add(3, 4)) // should log 7
+console.log(add(9, 102)) // should log 111
