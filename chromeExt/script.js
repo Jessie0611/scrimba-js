30c7aae6fe1b6aa268ec665d78abf6dc0601e165
Jessie0611 <imjustme0611@hotmail.com>
2025-07-15 16:33:25 -0500

    Make function more dynamic using arguments

--- chromeExt/script.js
+++ chromeExt/script.js
@@ -2,20 +2,17 @@
 //everything in local storage has to be a string, use JSON.stringify() and JSON.prase()
 //JSON.stringify() - turns array and turns it into string / JSON.parse - fetch array and turn into string
 
-
 // let myLeads = ["www.awesomelead.com"]
 // myLeads = JSON.stringify(myLeads)
 // myLeads = JSON.parse(myLeads)
 // myLeads.push("www.epiclead.com")
 // console.log(myLeads)
 
-
 //let myLeads = `["www.awesomelead.com"]`
 // 1. Turn the myLeads string into an array:                            myLeads = JSON.parse(myLeads)
 // 2. Push a new value to the array:                                    myLeads.push("www.epiclead.com")
 // 3. Turn the array into a string again:                               myLeads = JSON.stringify(myLeads)
 // 4. Console.log the string using typeof to verify that it's a string: console.log(typeof myLeads)
-
 
 //SAVE LEADS TO LOCAL STORAGE
 let myLeads = []
@@ -26,18 +23,32 @@ const deleteBtn = document.getElementById("delete-btn")
 
 //GET LEADS FROM LOCAL STORAGE :  JSON.parse()
 // Store it in a variable, leadsFromLocalStorage
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) //use const, it doesn't get changed
 //Check if leadsFromLocalStorage is truthy  If so, set myLeads to its value and call renderLeads()
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    renderLeads(myLeads) //pass in argument
+}
+
+//change to leads so it is not related to the myLeads global variable to make it more dynamic and reusable
+function render(leads) { //shows user what was found in local storagechange to leads so it is not related to the myLeads global variable
+    let listItems = ""   //
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
 }
 
 deleteBtn.addEventListener('dblclick', function ()){
     localStorage.clear() //clear local storage
     myLeads = [] // set to empty array
-    renderLeads() //clears DOM bc it renders out the newly empty array
+    render(myLeads) //clears DOM bc it renders out the newly empty array
 
 }
 // Log out the variable
@@ -48,22 +59,5 @@ inputBtn.addEventListener("click", function () {
     inputEl.value = ""
     // Save the myLeads array to localStorage   PS: remember JSON.stringify()
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-
-    // To verify that it works:
-    console.log(localStorage.getItem("myLeads"))
+    render(myLeads)
 })
-
-function renderLeads() { //shows user what was found in local storage 
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems
-}
