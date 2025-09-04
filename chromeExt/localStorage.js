979323410e265dfdd02f8fba1d33a73e91b9b7b7
Jessie0611 <imjustme0611@hotmail.com>
2025-09-04 17:51:30 -0500

    Chrome Extention

---
+++ chromeExt/localStorage.js
@@ -0,0 +1,8 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+
+//localStorage.setItem("myCredits", "100")
+
+localStorage.getItem("myCredits")
+console.log(myCredits)
